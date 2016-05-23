0bb504ddea0cd7c51c4323941bf6e56b69cd3432
mummick <mummick@mail.ru>
2016-05-23 12:35:49 +0300

    add testGit

---
+++ src/com/company/test_git.java
@@ -0,0 +1,10 @@
+package com.company;
+
+/**
+ * Created by lab on 23.05.16.
+ */
+public class test_git {
+    public static void main(String[] args) {
+        System.out.print("Testing GIT");
+    }
+}
