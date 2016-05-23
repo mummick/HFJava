6f8cdd8c1fb6b92c78e3e39cbafeb8dfd219437d
mummick <mummick@mail.ru>
2016-05-23 12:59:46 +0300

    readed some new pages

--- src/com/company/p294e1.java
+++ src/com/company/p294e1.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Date;
+
 /**
  * Created by lab on 23.05.16.
  */
@@ -8,6 +10,11 @@ public class p294e1 {
         String s = String.format("%, d", 1000000000);
         System.out.println(s);
         s = String.format("I have %,.2f bugs to fix", 123123.4567);
+        System.out.println(s);
+        s = String.format("%tc", new Date());
+        System.out.println(s);
+        Date today = new Date();
+        s = String.format("%tA, %<tB %<td", today);
         System.out.println(s);
     }
 }
